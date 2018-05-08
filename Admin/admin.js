5553e85b60fef2efcaf2221272fe443c73d3be85
mrojekdev <mrojekdev@gmail.com>
2018-05-08 15:14:14 +0700

    upggrade bootstrap 4

--- Admin/admin.js
+++ Admin/admin.js
@@ -4,143 +4,131 @@
             var validasi2 = $('#validasi2').val().length; 
             var validasi3 = $('#validasi3').val().length; 
             var validasi4 = $('#validasi4').val().length; 
-            // console.log("eek: " + validasi.toString() + validasi2.toString());   
+            console.log("eek: " + validasi.toString() + validasi2.toString());   
  
             if(validasi > 0 && validasi2 > 0 && validasi3 > 0 && validasi4 > 0)
             {
-                $("#ParentVal").attr('class','form-group has-feedback has-success');
-                $("#ParentVal2").attr('class','form-group has-feedback has-success');
-                $("#ParentVal3").attr('class','form-group has-feedback has-success');
-                $("#ParentVal4").attr('class','form-group has-feedback has-success');
-                $("#LogoVal").attr('class','glyphicon form-control-feedback glyphicon-ok');
-                $("#LogoVal2").attr('class','glyphicon form-control-feedback glyphicon-ok');
-                $("#LogoVal3").attr('class','glyphicon form-control-feedback glyphicon-ok');
-                $("#LogoVal4").attr('class','glyphicon form-control-feedback glyphicon-ok');
+                $("#ParentVal").attr('class','form-group has-success');
+                $("#validasi").attr('class','form-control form-control-success');
+                $("#ParentVal2").attr('class','form-group has-success');
+                $("#validasi2").attr('class','form-control form-control-success');
+                $("#ParentVal3").attr('class','form-group has-success');
+                $("#validasi3").attr('class','form-control form-control-success');
+                $("#ParentVal4").attr('class','form-group has-success');
+                $("#validasi4").attr('class','form-control form-control-success');
                 return true;
             }
             else
             {    
                 if(validasi > 0 )
                 {
-                    $("#ParentVal").attr('class','form-group has-feedback has-success');
-                    $("#LogoVal").attr('class','glyphicon form-control-feedback glyphicon-ok');
+                    $("#ParentVal").attr('class','form-group has-success');
+                    $("#validasi").attr('class','form-control form-control-success');
                 }
                 else
                 {
-                    $("#ParentVal").attr('class','form-group has-feedback has-error');
-                    $("#LogoVal").attr('class','glyphicon form-control-feedback glyphicon-remove');
+                    $("#ParentVal").attr('class','form-group has-danger');
+                    $("#validasi").attr('class','form-control form-control-danger');
                 }
-
-                if(validasi2 > 0)
+                if(validasi2 > 0 )
                 {
-                    $("#ParentVal2").attr('class','form-group has-feedback has-success');
-                    $("#LogoVal2").attr('class','glyphicon form-control-feedback glyphicon-ok');
-                }
-                else
-                {
-                    $("#ParentVal2").attr('class','form-group has-feedback has-error');
-                    $("#LogoVal2").attr('class','glyphicon form-control-feedback glyphicon-remove');
-                }
-
-                if(validasi3 > 0)
-                {
-                     $("#ParentVal3").attr('class','form-group has-feedback has-success');
-                     $("#LogoVal3").attr('class','glyphicon form-control-feedback glyphicon-ok');
+                    $("#ParentVal2").attr('class','form-group has-success');
+                    $("#validasi2").attr('class','form-control form-control-success');
                 }
                 else
                 {
-                    $("#ParentVal3").attr('class','form-group has-feedback has-error');
-                    $("#LogoVal3").attr('class','glyphicon form-control-feedback glyphicon-remove');
+                    $("#ParentVal2").attr('class','form-group has-danger');
+                    $("#validasi2").attr('class','form-control form-control-danger');
                 }
-
-                if(validasi4 > 0)
+                if(validasi3 > 0 )
                 {
-                    $("#ParentVal4").attr('class','form-group has-feedback has-success');
-                    $("#LogoVal4").attr('class','glyphicon form-control-feedback glyphicon-ok');
-                } 
+                    $("#ParentVal3").attr('class','form-group has-success');
+                    $("#validasi3").attr('class','form-control form-control-success');
+                }
                 else
                 {
-                    $("#ParentVal4").attr('class','form-group has-feedback has-error');
-                    $("#LogoVal4").attr('class','glyphicon form-control-feedback glyphicon-remove');
+                    $("#ParentVal3").attr('class','form-group has-danger');
+                    $("#validasi3").attr('class','form-control form-control-danger');
                 }
-                return false;
+                if(validasi4 > 0 )
+                {
+                    $("#ParentVal4").attr('class','form-group has-success');
+                    $("#validasi4").attr('class','form-control form-control-success');
+                }
+                else
+                {
+                    $("#ParentVal4").attr('class','form-group has-danger');
+                    $("#validasi4").attr('class','form-control form-control-danger');
+                }
             }
-
         });
     });
 
-
-  $(document).ready(function() {
+    $(document).ready(function() {
         $('.ManJadDok').submit(function() {
             var validasi5 = $('#validasi5').val().length; 
             var validasi6 = $('#validasi6').val().length; 
             var validasi7 = $('#validasi7').val().length; 
             var validasi8 = $('#validasi8').val().length; 
-            // console.log("eek: " + validasi.toString() + validasi2.toString());   
+            console.log("eek: " + validasi.toString() + validasi2.toString());   
  
             if(validasi5 > 0 && validasi6 > 0 && validasi7 > 0 && validasi8 > 0)
             {
-                $("#ParentVal5").attr('class','form-group has-feedback has-success');
-                $("#ParentVal6").attr('class','form-group has-feedback has-success');
-                $("#ParentVal7").attr('class','form-group has-feedback has-success');
-                $("#ParentVal8").attr('class','form-group has-feedback has-success');
-                $("#LogoVal5").attr('class','glyphicon form-control-feedback glyphicon-ok');
-                $("#LogoVal6").attr('class','glyphicon form-control-feedback glyphicon-ok');
-                $("#LogoVal7").attr('class','glyphicon form-control-feedback glyphicon-ok');
-                $("#LogoVal8").attr('class','glyphicon form-control-feedback glyphicon-ok');
+                $("#ParentVal5").attr('class','form-group has-success');
+                $("#validasi5").attr('class','form-control form-control-success');
+                $("#ParentVal6").attr('class','form-group has-success');
+                $("#validasi6").attr('class','form-control form-control-success');
+                $("#ParentVal7").attr('class','form-group has-success');
+                $("#validasi7").attr('class','form-control form-control-success');
+                $("#ParentVal8").attr('class','form-group has-success');
+                $("#validasi8").attr('class','form-control form-control-success');
                 return true;
             }
             else
             {    
                 if(validasi5 > 0 )
                 {
-                    $("#ParentVal5").attr('class','form-group has-feedback has-success');
-                    $("#LogoVal5").attr('class','glyphicon form-control-feedback glyphicon-ok');
+                    $("#ParentVal5").attr('class','form-group has-success');
+                    $("#validasi5").attr('class','form-control form-control-success');
                 }
                 else
                 {
-                    $("#ParentVal5").attr('class','form-group has-feedback has-error');
-                    $("#LogoVal5").attr('class','glyphicon form-control-feedback glyphicon-remove');
+                    $("#ParentVal5").attr('class','form-group has-danger');
+                    $("#validasi5").attr('class','form-control form-control-danger');
                 }
-
-                if(validasi6 > 0)
+                if(validasi6 > 0 )
                 {
-                    $("#ParentVal6").attr('class','form-group has-feedback has-success');
-                    $("#LogoVal6").attr('class','glyphicon form-control-feedback glyphicon-ok');
-                }
-                else
-                {
-                    $("#ParentVal6").attr('class','form-group has-feedback has-error');
-                    $("#LogoVal6").attr('class','glyphicon form-control-feedback glyphicon-remove');
-                }
-
-                if(validasi7 > 0)
-                {
-                     $("#ParentVal7").attr('class','form-group has-feedback has-success');
-                     $("#LogoVal7").attr('class','glyphicon form-control-feedback glyphicon-ok');
+                    $("#ParentVal6").attr('class','form-group has-success');
+                    $("#validasi6").attr('class','form-control form-control-success');
                 }
                 else
                 {
-                    $("#ParentVal7").attr('class','form-group has-feedback has-error');
-                    $("#LogoVal7").attr('class','glyphicon form-control-feedback glyphicon-remove');
+                    $("#ParentVal6").attr('class','form-group has-danger');
+                    $("#validasi6").attr('class','form-control form-control-danger');
                 }
-
-                if(validasi8 > 0)
+                if(validasi7 > 0 )
                 {
-                    $("#ParentVal8").attr('class','form-group has-feedback has-success');
-                    $("#LogoVal8").attr('class','glyphicon form-control-feedback glyphicon-ok');
-                } 
+                    $("#ParentVal7").attr('class','form-group has-success');
+                    $("#validasi7").attr('class','form-control form-control-success');
+                }
                 else
                 {
-                    $("#ParentVal8").attr('class','form-group has-feedback has-error');
-                    $("#LogoVal8").attr('class','glyphicon form-control-feedback glyphicon-remove');
+                    $("#ParentVal7").attr('class','form-group has-danger');
+                    $("#validasi7").attr('class','form-control form-control-danger');
                 }
-                return false;
+                if(validasi8 > 0 )
+                {
+                    $("#ParentVal8").attr('class','form-group has-success');
+                    $("#validasi8").attr('class','form-control form-control-success');
+                }
+                else
+                {
+                    $("#ParentVal8").attr('class','form-group has-danger');
+                    $("#validasi8").attr('class','form-control form-control-danger');
+                }
             }
-
         });
     });
-
 
 /* ERROR
 parent :has-error
